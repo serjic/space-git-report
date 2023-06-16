8b576d1b6e37c4d4c318aeb75826589a12379b82
sergey.shkredov <sergey.shkredov@jetbrains.com>
2023-06-16 15:45:40 +0200

    Create initial...

--- .space.kts
+++ .space.kts
@@ -3,9 +3,6 @@ job("Publish") {
         gitPush { enabled = false }
     }
 
-    gradlew("openjdk:11", "publish") {
-        env["APP_ID"] = "{{ project:app-id }}"
-        env["APP_SECRET"] = "{{ project:app-secret }}"
-
+    gradlew("openjdk:11", "publishImage") {
     }
 }
